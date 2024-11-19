2f595199c50a24d61ab195d6b16ee62fadc01f6b
daianefloresdeoliveira <156481606+daianefloresdeoliveira@users.noreply.github.com>
2024-11-19 13:36:55 -0500

    set the flow in the navigation tab ( Owners page)

--- app/src/main/java/com/example/finalproject/views/owner/OwnerMenuPage.java
+++ app/src/main/java/com/example/finalproject/views/owner/OwnerMenuPage.java
@@ -1,5 +1,6 @@
 package com.example.finalproject.views.owner;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,14 +10,41 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.finalproject.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class OwnerMenuPage extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_owner_menu_page);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.home) {
+                startActivity(new Intent(OwnerMenuPage.this, OwnerRestaurantPage.class));
+                return true;
+            } else if (id == R.id.menu) {
+                return true;
+            } else if (id == R.id.reservation) {
+                startActivity(new Intent(OwnerMenuPage.this, OwnerReservationsPage.class));
+                return true;
+            } else if (id == R.id.review) {
+                startActivity(new Intent(OwnerMenuPage.this, OwnerReviewsPage.class));
+                return true;
+            } else if (id == R.id.account) {
+                startActivity(new Intent(OwnerMenuPage.this, OwnerTabViewAccount.class));
+                return true;
+            }
+            return false;
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/finalproject/views/owner/OwnerRestaurantPage.java
+++ app/src/main/java/com/example/finalproject/views/owner/OwnerRestaurantPage.java
@@ -1,5 +1,6 @@
 package com.example.finalproject.views.owner;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,14 +10,41 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.finalproject.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class OwnerRestaurantPage extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_owner_restaurant_page);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.home) {
+                return true;
+            } else if (id == R.id.menu) {
+                startActivity(new Intent(OwnerRestaurantPage.this, OwnerMenuPage.class));
+                return true;
+            } else if (id == R.id.reservation) {
+                startActivity(new Intent(OwnerRestaurantPage.this, OwnerReservationsPage.class));
+                return true;
+            } else if (id == R.id.review) {
+                startActivity(new Intent(OwnerRestaurantPage.this, OwnerReviewsPage.class));
+                return true;
+            } else if (id == R.id.account) {
+                startActivity(new Intent(OwnerRestaurantPage.this, OwnerTabViewAccount.class));
+                return true;
+            }
+            return false;
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
