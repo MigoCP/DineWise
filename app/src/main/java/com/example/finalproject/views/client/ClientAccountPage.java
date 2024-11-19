2f288e347e87ec627b94613cc7494d3edf8a75aa
QueenSarah21 <128749996+Queen-Sarah21@users.noreply.github.com>
2024-11-18 23:38:47 -0500

    worked on the client profile functionality

--- app/src/main/java/com/example/finalproject/views/client/ClientAccountPage.java
+++ app/src/main/java/com/example/finalproject/views/client/ClientAccountPage.java
@@ -7,25 +7,34 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.finalproject.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
-public class ClientAccountPage extends AppCompatActivity implements View.OnClickListener{
+public class ClientAccountPage extends AppCompatActivity implements View.OnClickListener, ValueEventListener, OnSuccessListener, OnFailureListener, OnCompleteListener {
     TextView tvFullName, tvJoinDate, tvContributions;
     ImageButton imBtnCity, imBtnWebsite, imBtnEdit, btnWriteReview, btnUploadPhoto,
             btnMyReserves, btnFavoriteRestaurants;
     Button btnView;
 
-    FirebaseDatabase database;
     DatabaseReference sRef;
-    FirebaseAuth ownerAuth;
+    FirebaseAuth clientAuth;
 
 
     @Override
@@ -39,15 +48,77 @@ public class ClientAccountPage extends AppCompatActivity implements View.OnClick
             return insets;
         });
         initialize();
+
+        clientAuth = FirebaseAuth.getInstance();
+        // Check if user is authenticated
+        if (clientAuth.getCurrentUser() != null) {
+            String clientId = clientAuth.getCurrentUser().getUid();
+            sRef = FirebaseDatabase.getInstance().getReference("users").child(clientId);
+
+            // Attach single value event listener
+            sRef.addListenerForSingleValueEvent(this);
+        } else {
+            // Handle cases where the user is not logged in
+            finish();
+        }
     }
 
     public void initialize() {
         tvFullName = findViewById(R.id.tvFullName);
         tvJoinDate = findViewById(R.id.tvJoinDate);
+        tvContributions = findViewById(R.id.tvContributions);
+        imBtnCity = findViewById(R.id.imBtnCity);
+        imBtnWebsite = findViewById(R.id.imBtnWebsite);
+        imBtnEdit = findViewById(R.id.imBtnEdit);
+        btnWriteReview = findViewById(R.id.btnWriteReview);
+        btnUploadPhoto = findViewById(R.id.btnUploadPhoto);
+        btnMyReserves = findViewById(R.id.btnMyReserves);
+        btnFavoriteRestaurants = findViewById(R.id.btnFavoriteRestaurants);
+        btnView = findViewById(R.id.btnView);
+
+        imBtnCity.setOnClickListener(this);
+        imBtnWebsite.setOnClickListener(this);
+        imBtnEdit.setOnClickListener(this);
+        btnWriteReview.setOnClickListener(this);
+        btnUploadPhoto.setOnClickListener(this);
+        btnMyReserves.setOnClickListener(this);
+        btnFavoriteRestaurants.setOnClickListener(this);
+        btnView.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+        if (dataSnapshot.exists()) {
+            // Retrieve data from the database
+            String name = dataSnapshot.child("name").getValue(String.class);
+            // Update the UI
+            if (name != null) tvFullName.setText(name);
+
+        }
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError error) {
+
+    }
+
+    @Override
+    public void onComplete(@NonNull Task task) {
+
+    }
+
+    @Override
+    public void onFailure(@NonNull Exception e) {
+
+    }
+
+    @Override
+    public void onSuccess(Object o) {
 
     }
 }
